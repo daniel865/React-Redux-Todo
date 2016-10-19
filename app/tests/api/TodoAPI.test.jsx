10b11f50bc9102b7e0ca7fca9c19de8d10b2a78e
daniel865 <daniel865live@gmail.com>
2016-10-19 14:29:35 -0500

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -61,4 +61,49 @@ describe('TodoAPI', () => {
 
 	});
 
+	describe('filterTodos', () => {
+
+		let todos = [
+			{
+				id: 1,
+				text: 'Some text here',
+				completed: true
+			},{
+				id: 2,
+				text: 'other text here',
+				completed: false
+			},{
+				id: 3,
+				text: 'Some text here',
+				completed: true
+			}
+		];
+
+		it('should return all items if showCompleted is true', () => {
+			let filterTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filterTodos.length).toBe(3);
+		});
+
+		it('should return non-completed todos when showCompleted is false', () => {
+			let filterTodos = TodoAPI.filterTodos(todos, false, '');
+			expect(filterTodos.length).toBe(1);
+		});
+
+		it('should sort by completed status', () => {
+			let filterTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filterTodos[0].completed).toBe(false);
+		});
+
+		it('should filter todos by searchText', () => {
+			let filterTodos = TodoAPI.filterTodos(todos, true, 'some');
+			expect(filterTodos.length).toBe(2);
+		});
+
+		it('should return all todos if searchText is empty', () => {
+			let filterTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filterTodos.length).toBe(3);
+		});
+
+	});
+
 });

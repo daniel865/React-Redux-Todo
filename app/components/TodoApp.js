ad38a7c9a6f9c348b56931f34bf057748cb48692
daniel865 <daniel865live@gmail.com>
2016-10-19 13:30:41 -0500

    Add localStorage support

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import uuid from 'node-uuid';
+
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-import uuid from 'node-uuid';
+import TodoAPI from 'TodoAPI';
 
 export default class TodoApp extends Component {
 
@@ -12,29 +14,15 @@ export default class TodoApp extends Component {
 		this.state = {
 			showCompleted: false,
       		searchText: '',
-			todos: [
-				{
-					id: uuid(),
-					text: 'walk the dog',
-					completed: false
-				},{
-					id: uuid(),
-					text: 'Clean the yard',
-					completed: true
-				}, {
-					id: uuid(),
-					text: 'Pay the bills',
-					completed: false
-				}, {
-					id: uuid(),
-					text: 'Do the homework',
-					completed: true
-				}
-			]
+			todos: TodoAPI.getTodos()
 		}
 		this.handleAddTodo = this.handleAddTodo.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
 		this.handleToggle = this.handleToggle.bind(this);
+	}
+
+	componentDidUpdate(){
+		TodoAPI.setTodos(this.state.todos);
 	}
 
 	handleAddTodo(text){
